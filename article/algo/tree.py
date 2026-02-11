b027fdc137b8a9a38d5e0ffa87d66ce1ba1bbd0a
Jean Fromentin <jean.fromentin@univ-littoral.fr>
2026-02-11 12:24:58 +0100

    Continue on algorithms

--- article/algo/tree.py
+++ article/algo/tree.py
@@ -1,6 +1,28 @@
 def intersection(L1, L2):
     pass
 
+def set_to_int(X):
+    res = 0
+    m = max(X)
+    p = 1
+    for i in range(1, m + 1):
+        if i in X:
+            res += p
+        p *= 2
+    return res
+
+def code_to_set(c):
+    res = []
+    i = 1
+    p = 1
+    while c != 0:
+        if (c % (2 * p)) == p:
+            res.append(i)
+            c -= p
+        i += 1
+        p *= 2
+    return res
+
 class Tree:
 
     def __init__(self, c):
@@ -11,14 +33,14 @@ class Tree:
     def __repr__(self):
         return repr(self.c)
 
-    def left_forest(self, amin, amax, i):
+    def get_forest(self, amin, amax, i):
         A = set(range(amin, amax + 1))
         c = []
         c.append(set([]))
         h = 0
         for j in range(i, self.h + 1):
             X = A.intersection(self.c[j])
-            X = set([a - amin for a in X])
+            X = set([x - amin + 1 for x in X])
             if X != c[h]:
                 h += 1
                 c.append(X)
@@ -28,12 +50,39 @@ class Tree:
     def left_comb_decomposition(self):
         R = []
         A = set(range(1, self.n + 1))
-        for i in range(1, self.h):
+        for i in range(1, self.h + 1):
             X = A.intersection(self.c[i])
             if len(X) > 0:
-                F = self.left_forest(min(X) + 1, max(A), i)
+                F = self.get_forest(min(X) + 1, max(A), i)
                 R = R + [F]
                 A = set(range(1, min(X)))
         return R
 
-T = Tree([[],[4,6], [4,5,6], [1,4,5,6], [1,2,3,4,5,6]])
+    def right_comb_decomposition(self):
+        R = []
+        A = set(range(1, self.n + 1))
+        for i in range(1, self.h + 1):
+            X = A.intersection(self.c[i])
+            if len(X) > 0:
+                F = self.get_forest(min(A), max(X) - 1, i)
+                R = R + [F]
+                A = set(range(max(X) + 1, self.n + 1))
+        return R
+    
+    def code(self):
+        return [set_to_int(self.c[i]) for i in range(self.h, 0, -1)]
+
+def tree_from_code(C):
+    s = []
+    s.append(set([]))
+    for i in range(len(C) - 1, -1, -1):
+        s.append(code_to_set(C[i]))
+    return Tree(s)
+        
+    
+
+    
+T1 = Tree([[], [4,6], [4,5,6], [1,4,5,6], [1,2,3,4,5,6]])
+T2 = Tree([[], [2], [2,4,5], [2,3,4,5], [1,2,3,4,5]])
+
+T = tree_from_code([511, 510,  494, 302, 14, 2])
